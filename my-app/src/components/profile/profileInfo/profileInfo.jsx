acb254de15e8872a467c9c9ae9b1815168851fac
kalynov <temachempion@gmail.com>
2024-03-16 00:47:55 +0100

    fix img

--- my-app/src/components/profile/profileInfo/profileInfo.jsx
+++ my-app/src/components/profile/profileInfo/profileInfo.jsx
@@ -6,7 +6,7 @@ import classes from './profileInfo.module.css';
 const ProfileInfo= () =>(
     <div>
        <div>
-        <img src = "https://asiamountains.net/upload/slide/slide-1960x857-07.jpg"></img>
+        <img alt='' src = "https://asiamountains.net/upload/slide/slide-1960x857-07.jpg"></img>
        </div>
        <div>
          ava + description
